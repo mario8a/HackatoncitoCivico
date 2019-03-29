d7d3599911d2b08955ee803628ca81971f9d36a5
Mario Ochoa <mario8ato@hotmail.com>
2019-03-28 21:25:37 -0600

    todo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,78 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+
+  constructor(public alertController: AlertController) {}
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Formulario',
+      message: 'Calificacion: <strong>***</strong>',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'JUSTICIA'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+}
